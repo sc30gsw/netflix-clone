0e6465f61479583fd90170c35bc1009b2fe1b8c2
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-08-29 18:06:54 +0900

    ログインユーザーを表示・サインアウトの実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { signOut } from 'next-auth/react'
+
+import useCurrentUser from './hooks/useCurrentUser'
+
 const Home = () => {
+  const router = useRouter()
+
+  const { data: user } = useCurrentUser()
+
   return (
     <>
-      <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+      <h1 className="text-4xl text-green-500">Netflix Clone</h1>
+      <p className="text-white">Logged in as: {user?.currentUser?.email}</p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        Logout!
+      </button>
     </>
   )
 }
