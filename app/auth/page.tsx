2b4cf5ff19fb10b65bf129098125f5589b8e821b
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-09-01 07:51:31 +0900

    不要なコンポーネントの削除

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -191,7 +191,6 @@ const Auth = () => {
               </div>
               <button
                 type="submit"
-                // onClick={variant === 'login' ? login : register}
                 className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
               >
                 {variant === 'login' ? 'Login' : 'Sign up'}
