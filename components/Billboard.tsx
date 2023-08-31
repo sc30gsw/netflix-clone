442b381ce04c3b0fc10a0d6d462881f82e147b89
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-08-31 23:06:20 +0900

    fontSizeの修正

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -41,7 +41,7 @@ const Billboard = async () => {
         </p>
         <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
           <PlayButton movieId={randomMovies[0]?.id} />
-          <button className="h-10 bg-white text-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg flex flex-row items-center hover:bg-opacity-20 transition">
+          <button className="h-10 bg-white text-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-base lg:text-lg flex flex-row items-center hover:bg-opacity-20 transition font-bold">
             <AiOutlineInfoCircle className="mr-1" />
             More Info
           </button>
